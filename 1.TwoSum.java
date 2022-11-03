9ce58b61510f5e3ffcaddc28ed5d091169e8d920
Srishti <75185963+Srishti-Kumar@users.noreply.github.com>
2022-11-03 07:32:26 +0530

    Hashmap Optimal
    https://leetcode.com/problems/two-sum/

--- 1.TwoSum.java
+++ 1.TwoSum.java
@@ -2,16 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] arr = new int[2];
-        for(int i = 0;i<nums.length;i++) {
-                for(int j = i+1;j < nums.length;j++) {
-                    if (nums[i] + nums[j] == target) {
-                        arr[0] = i;
-                        arr[1] = j;
-
-                    }
-                }
+        int n = nums.length;
+        int[] result = new int[2];
+        
+        HashMap<Integer,Integer> hmap = new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(hmap.containsKey(nums[i])){
+                int index1 = hmap.get(nums[i]);
+                result[0] = index1;
+                result[1] = i;
+                
+            }else{
+                hmap.put(target - nums[i],i); //7,0
+            }
         }
-        return arr;
     }
 }
